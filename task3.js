8684bd4bd611982821bd35fb13c3d468b6d87275
Izaaz Waskito <116268166+izaazwaskito@users.noreply.github.com>
2023-06-07 16:16:10 +0700

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,34 +1,38 @@
-function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray){
-    if(nilaiAwal < nilaiAkhir){
-      if(typeof nilaiAwal === "number" && typeof nilaiAkhir === "number" && Array.isArray(dataArray)){
-          if(dataArray.length>5){
-            filterArray(nilaiAwal,nilaiAkhir,dataArray)
-          }else{
-            console.log("Jumlah angka dalam dataArray harus lebih dari 5")
-          }
-      }
-    }else{
-      console.log("Nilai akhir harus lebih besar dari nilai awal")
+function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
+    validasi(nilaiAwal, nilaiAkhir, dataArray)
+}
+
+function validasi(nilaiAwal, nilaiAkhir, dataArray) {
+    if (nilaiAwal < nilaiAkhir) {
+        if (typeof nilaiAwal === "number" && typeof nilaiAkhir === "number" && Array.isArray(dataArray)) {
+            if (dataArray.length > 5) {
+                filterArray(nilaiAwal, nilaiAkhir, dataArray)
+            } else {
+                console.log("Jumlah angka dalam dataArray harus lebih dari 5")
+            }
+        } else {
+            console.log("nilaiAwal/nilaiAkhir ada data tidak bernilai number")
+        }
+    } else {
+        console.log("Nilai akhir harus lebih besar dari nilai awal")
     }
-  }
-  
-  function filterArray(nilaiAwal, nilaiAkhir, dataArray){
+}
+
+
+function filterArray(nilaiAwal, nilaiAkhir, dataArray) {
     const hasil = dataArray.filter((item) => {
         if (item >= nilaiAwal && item <= nilaiAkhir) {
-          return item;
+            return item;
         }
-      })
-      .sort((a, b) => a - b);
-  
+    }).sort((a, b) => a - b);
     if (hasil.length > 0) {
-      console.log(hasil);
+        console.log(hasil);
     } else {
-      console.log("Nilai tidak ditemukan");
+        console.log("Nilai tidak ditemukan");
     }
-  }
-  
-  seleksiNilai(5, 20 , [2, 25, 4, 14, 17, 30, 8])
-  seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8])
-  seleksiNilai(5, 17 , [2, 25, 4])
-  seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18])
-  
+}
+
+seleksiNilai(5, 20 , [2, 25, 4, 14, 17, 30, 8])
+seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8])
+seleksiNilai(5, 17 , [2, 25, 4])
+seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18])
