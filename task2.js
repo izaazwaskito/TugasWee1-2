5d0e5ca649d0c0a1829b8669ab1a54fa060c0d42
Izaaz Waskito <116268166+izaazwaskito@users.noreply.github.com>
2023-06-07 16:10:45 +0700

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,17 +1,51 @@
 const name = [
-  "Abigail", "Alexandra", "Alison",
-  "Amanda", "Angela", "Bella",
-  "Carol", "Caroline", "Carolyn",
-  "Deirdre", "Diana", "Elizabeth",
-  "Ella", "Faith", "Olivia", "Penelope"]
+    "Abigail",
+    "Alexandra",
+    "Alison",
+    "Amanda",
+    "Angela",
+    "Bella",
+    "Carol",
+    "Caroline",
+    "Carolyn",
+    "Deirdre",
+    "Diana",
+    "Elizabeth",
+    "Ella",
+    "Faith",
+    "Olivia"
+]
 
-const searchName = (str, limit,  callback) =>{
-  const hasil = name.filter((name)=>
-  name.toLowerCase().indexOf(str.toLowerCase())!=-1)
-  callback(hasil, limit)
+function searchName(str, limit) {
+    validasi(str, limit)
 }
-callback = (hasil,limit) =>{
-  hasil=hasil.slice(0,limit)
-  console.log(hasil)
+function cekNama(str, limit) {
+    let hasil = name.filter((name) => name.toLowerCase().indexOf(str.toLowerCase()) != -1)
+    hasil = hasil.slice(0, limit)
+    callback(hasil)
 }
-searchName("an",3,callback)
+function callback(hasil) {
+    console.log(hasil);
+}
+
+function validasi(str, limit) {
+    try {
+        if (typeof str !== "string") 
+            throw "nilai str harus string";
+        
+        if (isNaN(limit)) 
+            throw "nilai limit harus number";
+        
+        cekNama(str, limit)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+searchName("an", 3, callback);
+
+searchName(3, 3, callback);
+
+searchName("an", "an", callback);
+// cekNama("an",3,callback)
+
